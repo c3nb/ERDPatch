861ffda455be45726ef426d2ddbd86735d72a39b
C#Newbie <dongsu45681111@gmail.com>
2023-09-15 23:15:22 +0900

    RDStringPatch

--- ERDPatch/Main.cs
+++ ERDPatch/Main.cs
@@ -19,17 +19,15 @@ namespace ERDPatch
             Mod = modEntry;
             Logger = modEntry.Logger;
             Harmony = new Harmony(modEntry.Info.Id);
-            EnumPatcher<CamMovementType>.SetField("Global", 6);
-            EnumPatcher<CamMovementType>.SetField("Player", 33);
-            EnumPatcher<CamMovementType>.RemoveField("LastPosition");
-            EnumPatcher<CamMovementType>.AddField("FUCKKKK", 2332);
-            EnumPatcher<CamMovementType>.AddField("FUCKINGSDJIJ", 345436);
-            foreach (var e in Enum.GetValues(typeof(CamMovementType)))
-                Logger.Log($"{e} - {(int)e}");
         }
+        #region EnumPatch
         public static readonly MethodInfo GCVAN = typeof(Enum).GetMethod("GetCachedValuesAndNames", (BindingFlags)15420);
         public static readonly MethodInfo GEN = Type.GetType("System.RuntimeType").GetMethod("GetEnumName", (BindingFlags)15420);
         public static readonly AccessTools.FieldRef<object, string[]> VAN_Names = AccessTools.FieldRefAccess<string[]>(typeof(Enum).GetNestedType("ValuesAndNames", (BindingFlags)15420), "Names");
         public static readonly AccessTools.FieldRef<object, ulong[]> VAN_Values = AccessTools.FieldRefAccess<ulong[]>(typeof(Enum).GetNestedType("ValuesAndNames", (BindingFlags)15420), "Values");
+        #endregion
+        #region RDStringPatch
+        public static readonly MethodInfo GWC = typeof(RDString).GetMethod("GetWithCheck", (BindingFlags)15420);
+        #endregion
     }
 }
--- ERDPatch/RDStringPatcher.cs
+++ ERDPatch/RDStringPatcher.cs
@@ -1,16 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
 
 namespace ERDPatch
 {
     public static class RDStringPatcher
     {
-        public static void AddString()
+        private static Dictionary<string, Text> texts;
+        public static void AddString(string key, string str)
         {
-
+            texts.Add(key, new Text(str));
+        }
+        public static void RemoveString(string key)
+        {
+            texts[key] = new Text(key);
+        }
+        public static void SetString(string key, string str)
+        {
+            texts[key] = new Text(str);
+        }
+        static RDStringPatcher()
+        {
+            texts = new Dictionary<string, Text>();
+            Main.Harmony.Patch(Main.GWC, new HarmonyMethod(typeof(RDStringPatcher).GetMethod(nameof(GWCPatch), (BindingFlags)15420)));
+        }
+        private static bool GWCPatch(string key, out bool exists, Dictionary<string, object> parameters, ref string __result)
+        {
+            exists = false;
+            if (texts.TryGetValue(key, out var text))
+            {
+                exists = !text.removed;
+                __result = text.text ?? string.Empty;
+                if (exists) __result = RDString.ReplaceParameters(__result, parameters);
+                return false;
+            }
+            return true;
+        }
+        struct Text
+        {
+            public Text(string s)
+            {
+                text = s;
+                removed = false;
+            }
+            public Text(bool removed)
+            {
+                text = null;
+                this.removed = removed;
+            }
+            public string text;
+            public bool removed;
         }
     }
 }
